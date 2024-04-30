4fc4bdaf0b21f082f6f88e5493efa340712920dd
GsLogimaker <gslogimaker@gmail.com>
2024-04-30 12:51:25 -0500

    Add basic documentation to entity.gd

--- addons/glecs/gd/entity.gd
+++ addons/glecs/gd/entity.gd
@@ -1,55 +1,83 @@
 
+## A reference to an entity.
+##
+## TODO: Explain conversions from Variant to GlecsEntity
+
 class_name GlecsEntity extends _GlecsEntity
 
+## Called when the script is registered with Glecs.
 static func _registered(world:GlecsWorld) -> void:
 	pass
 
-## Creates a new entity
+## Creates a new entity.
 static func spawn(world:GlecsWorld = null) -> GlecsEntity:
 	return _GlecsEntity._spawn(world)
 
-## Creates a reference to an existing entity from the given [Varaint].
+## Creates a reference to an existing entity from the
+## given [Variant].
 static func from(entity:Variant, world:GlecsWorld = null) -> GlecsEntity:
 	return _GlecsEntity._from(entity, world)
 
-func add_component(component:Variant, data:Variant=null) -> void:
-	_add_component(component, data)
+## Adds component data to this entity, with optional custom default value.
+func add_component(component:Variant, default_value:Variant=null) -> void:
+	_add_component(component, default_value)
 
+## Returns a reference to a component.
 func get_component(component:Variant) -> GlecsComponent:
 	return _get_component(component)
 
+## Removes component data from this entity.
 func remove_component(component: Variant) -> void:
 	_remove_component(component)
 
+## Deletes this entity from the ECS world.
+##
+## Note: this does not delete the [GlecsEntity] object, which is
+## a reference counted reference to the entity.
 func delete() -> void:
 	_delete()
 
-func add_entity(entity: Variant) -> void:
-	_add_entity(entity)
+## Adds an other entity as a tag or relationship to this entity.
+func add_entity(tag: Variant) -> void:
+	_add_entity(tag)
 
-func has_entity(entity: Variant) -> bool:
-	return _has_entity(entity)
+## Returns true if this entity has the given tag or relationship.
+func has_entity(tag: Variant) -> bool:
+	return _has_entity(tag)
 
-func remove_entity(entity: Variant) -> void:
-	_remove_entity(entity)
+## Removes the given tag or relationship with this entity.
+func remove_entity(tag: Variant) -> void:
+	_remove_entity(tag)
 
+## Returns the ID of this entity according to its world.
 func get_id() -> int:
 	return _get_id()
 
+## Returns the name of this entity.
 func get_name() -> String:
 	return _get_name()
 
+## Sets the name of this entity.
 func set_name(value: String) -> void:
 	_set_name(value)
 
-func add_relation(relation: Variant, with_entity: GlecsEntity) -> void:
+## Adds to this entity a relationship between the two given entites.
+func add_relation(relation: Variant, with_entity: Variant) -> void:
 	_add_relation(relation, with_entity)
 
-func remove_relation(relation: Variant, with_entity: GlecsEntity) -> void:
+## Removes from this entity a relationship between the two given entites.
+func remove_relation(relation: Variant, with_entity: Variant) -> void:
 	_remove_relation(relation, with_entity)
 
+## Returns true if this entity reference is valid.
+##
+## An entity reference is valid if the following are true:
+## - The ID of the reference is a real entity in the world.
+## - The entity is not deleted.
+## - The world is not delete.
 func is_valid() -> bool:
 	return _is_valid()
 
-func get_world() -> _GlecsWorld:
+## Returns the world object this entity is in.
+func get_world() -> GlecsWorld:
 	return _get_world()
