01ece7c58324e4c2cab883842d5c2cb52e6922f1
GsLogiMaker <87501052+GsLogiMaker@users.noreply.github.com>
2024-05-05 21:25:09 -0500

    Delete glecs directory

--- glecs/bin/x86_64-unknown-linux-gnu/release/deps/once_cell-fb87b4bd1fc257e7.d
+++
@@ -1,9 +0,0 @@
-/home/runner/work/glecs_godot_plugin/glecs_godot_plugin/./addons/glecs/bin/x86_64-unknown-linux-gnu/release/deps/libonce_cell-fb87b4bd1fc257e7.rmeta: /home/runner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/once_cell-1.19.0/src/lib.rs /home/runner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/once_cell-1.19.0/src/imp_std.rs /home/runner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/once_cell-1.19.0/src/race.rs
-
-/home/runner/work/glecs_godot_plugin/glecs_godot_plugin/./addons/glecs/bin/x86_64-unknown-linux-gnu/release/deps/libonce_cell-fb87b4bd1fc257e7.rlib: /home/runner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/once_cell-1.19.0/src/lib.rs /home/runner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/once_cell-1.19.0/src/imp_std.rs /home/runner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/once_cell-1.19.0/src/race.rs
-
-/home/runner/work/glecs_godot_plugin/glecs_godot_plugin/./addons/glecs/bin/x86_64-unknown-linux-gnu/release/deps/once_cell-fb87b4bd1fc257e7.d: /home/runner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/once_cell-1.19.0/src/lib.rs /home/runner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/once_cell-1.19.0/src/imp_std.rs /home/runner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/once_cell-1.19.0/src/race.rs
-
-/home/runner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/once_cell-1.19.0/src/lib.rs:
-/home/runner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/once_cell-1.19.0/src/imp_std.rs:
-/home/runner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/once_cell-1.19.0/src/race.rs:
