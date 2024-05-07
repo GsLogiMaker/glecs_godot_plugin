d90a65efd55517d1ddfa6b121ea22d675e53770e
Gabriel Schwab <gslogimaker@gmail.com>
2024-05-06 22:07:11 -0500

    Switch to Python for _build_for_nightly step in unittests.yml

---
+++ addons/glecs/_build_for_nightly.py
@@ -0,0 +1,24 @@
+
+import os
+import regex
+
+os.remove("glecs.gdextension")
+os.rename("glecs.gdextension.release", "glecs.gdextension")
+
+name_ptrn = regex.compile(r"""name\s*=\s*\"(.*)\"""")
+version_ptrn = regex.compile(r"""version\s*=\s*\"(.*)\"""")
+
+with open("plugin.cfg", "r+") as f:
+	txt = f.read()
+	
+	start, end = name_ptrn.search(txt).span(1)
+	txt = txt[:start] + "GlecsNightly" + txt[end:]
+
+	start, end = version_ptrn.search(txt).span(1)
+	txt = txt[:end] + "-nightly" + txt[end:]
+	
+	f.seek(0)
+	f.write(txt)
+	f.truncate()
+
+exit(0)
