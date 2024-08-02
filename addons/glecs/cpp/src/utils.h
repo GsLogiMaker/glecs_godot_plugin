d85d8eaa45b44039fa5dbb4fa00a905582067a40
GsLogimaker <gslogimaker@gmail.com>
2024-08-02 15:31:53 -0500

    Add Result class to utils.h

--- addons/glecs/cpp/src/utils.h
+++ addons/glecs/cpp/src/utils.h
@@ -9,4 +9,55 @@
 	UtilityFunctions::push_error(__VA_ARGS__); \
 	return return_value;
 
+template <typename T, typename E>
+class Result {
+
+public:
+	Result(T value_) {
+		_is_ok = true;
+		value = value_;
+	}
+	Result(E error_) {
+		_is_ok = false;
+		error = error_;
+	}
+	~Result() {
+		if (is_ok()) {
+			value.~T();
+		} else {
+			error.~E();
+		}
+	}
+
+	static Result Ok(T value) {
+		return Result(value);
+	}
+	static Result Err(E error) {
+		return Result(error);
+	}
+
+	bool is_ok() {
+		return _is_ok;
+	}
+	T unwrap() {
+		if (!is_ok()) {
+			throw "Tried to convert Error to Ok";
+		}
+		return value;
+	};
+	E unwrap_err() {
+		if (is_ok()) {
+			throw "Tried to convert Ok to Error";
+		}
+		return error;
+	};
+
+private:
+	bool _is_ok;
+	union {
+        T value;
+        E error;
+    };
+};
+
 #endif
