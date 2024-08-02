a1ee0890e46f5a4eca5e994c1cbcc69f25663133
GsLogimaker <gslogimaker@gmail.com>
2024-08-01 19:41:40 -0500

    Add coerce_id to GlWorld and used it wherever IDs were received as arguments

--- addons/glecs/cpp/src/world.cpp
+++ addons/glecs/cpp/src/world.cpp
@@ -1,5 +1,7 @@
 
 #include "world.h"
+#include "entity.h"
+#include "utils.h"
 
 #include <flecs.h>
 #include <godot_cpp/core/class_db.hpp>
@@ -15,6 +17,50 @@ GlWorld::GlWorld() {
 
 GlWorld::~GlWorld() {
 	ecs_fini(_raw);
+}
+
+ecs_entity_t GlWorld::coerce_id(Variant value) {
+	String string;
+	godot::CharString str;
+	Vector2i vec;
+	Object* obj;
+	switch (value.get_type()) {
+		case Variant::INT:
+			return value;
+		case Variant::VECTOR2I:
+			vec = Vector2i(value);
+			return ecs_pair(vec.x, vec.y);
+		case Variant::OBJECT:
+			obj = value;
+			if (obj == nullptr) {
+				ERR(0,
+					"Null objects can't be coerced to valid entity IDs"
+				);
+			}
+			if (obj->is_class(GlEntity::get_class_static())) {
+				return ((GlEntity*) obj)->get_id();
+			}
+			ERR(0,
+				"Objects of type ",
+				obj->get_class(),
+				" can't be coerced to valid entity IDs"
+			);
+			break;
+		case Variant::STRING:
+		case Variant::STRING_NAME:
+		case Variant::NODE_PATH:
+			string = value;
+			str = string.utf8();
+			return ecs_lookup_path_w_sep(raw(), 0, str, "/", "/root/", false);
+		default:
+			break;
+	};
+
+	ERR(0,
+		"Variants of type ",
+		value.get_type_name(value.get_type()),
+		" can't be coerced to valid entity IDs"
+	);
 }
 
 void GlWorld::progress(double delta) {
@@ -38,6 +84,7 @@ ecs_world_t * GlWorld::raw() {
 }
 
 void GlWorld::_bind_methods() {
+	godot::ClassDB::bind_method(D_METHOD("coerce_id", "entity"), &GlWorld::coerce_id);
 	godot::ClassDB::bind_method(D_METHOD("start_rest_api"), &GlWorld::start_rest_api);
 	godot::ClassDB::bind_method(D_METHOD("progress", "delta"), &GlWorld::progress);
 }
